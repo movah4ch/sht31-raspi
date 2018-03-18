1e316c67c4e49ec47e2dd95f993d66aad867d4eb
movah4ch <dugf0903@163.com>
2018-03-18 16:37:43 +0800

    second commint

--- sht31.py
+++ sht31.py
@@ -1,77 +1,34 @@
-# -*- coding: utf-8 -*-  
-  
-import RPi.GPIO as gpio  
-import time  
-  
-PORT=13 #使用的树莓派的PIN13也就是GPIO27，根据自己连接位置更改此参数  
-  
-gpio.setwarnings(False)  
-gpio.setmode(gpio.BOARD)  
-time.sleep(1)  
-data=[]  
-def delay(i): #20*i usdelay  
-    a=0  
-    for j in range(i):  
-        a+1  
-j=0  
-  
-#start work  
-gpio.setup(PORT,gpio.OUT)  
-gpio.output(PORT,gpio.LOW)  
-time.sleep(0.02)  
-gpio.output(PORT,gpio.HIGH)  
-i=1  
-  
-#wait to response  
-gpio.setup(PORT,gpio.IN)  
-  
-while gpio.input(PORT)==1:  
-    continue  
-  
-while gpio.input(PORT)==0:  
-    continue  
-  
-while gpio.input(PORT)==1:  
-        continue  
-  
-#get data  
-while j<40:  
-    k=0  
-    while gpio.input(PORT)==0:  
-        continue  
-      
-    while gpio.input(PORT)==1:  
-        k+=1  
-        if k>100:break  
-    if k<3:  
-        data.append(0)  
-    else:  
-        data.append(1)  
-    j+=1  
-  
-#get temperature  
-humidity_bit=data[0:8]  
-humidity_point_bit=data[8:16]  
-temperature_bit=data[16:24]  
-temperature_point_bit=data[24:32]  
-check_bit=data[32:40]  
-  
-humidity=0  
-humidity_point=0  
-temperature=0  
-temperature_point=0  
-check=0  
-  
-  
-for i in range(8):  
-    humidity+=humidity_bit[i]*2**(7-i)  
-    humidity_point+=humidity_point_bit[i]*2**(7-i)  
-    temperature+=temperature_bit[i]*2**(7-i)  
-    temperature_point+=temperature_point_bit[i]*2**(7-i)  
-    check+=check_bit[i]*2**(7-i)  
-  
-tmp=humidity+humidity_point+temperature+temperature_point  
-if check==tmp:  
-    print ("temperature is ", temperature,"wet is ",humidity,"%" )
-else:  
-    print ("something is worong the humidity,humidity_point,temperature,temperature_point,check is", humidity,humidity_point,temperature,temperature_point,check)
+# Distributed with a free-will license.
+# Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
+# SHT31
+# This code is designed to work with the SHT31_I2CS I2C Mini Module available from ControlEverything.com.
+# https://www.controleverything.com/content/Humidity?sku=SHT31_I2CS#tabs-0-product_tabset-2
+
+import smbus
+import time
+
+# Get I2C bus
+bus = smbus.SMBus(1)
+
+# SHT31 address, 0x44(68)
+# Send measurement command, 0x2C(44)
+#		0x06(06)	High repeatability measurement
+bus.write_i2c_block_data(0x44, 0x2C, [0x06])
+
+time.sleep(0.5)
+
+# SHT31 address, 0x44(68)
+# Read data back from 0x00(00), 6 bytes
+# Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC
+data = bus.read_i2c_block_data(0x44, 0x00, 6)
+
+# Convert the data
+temp = data[0] * 256 + data[1]
+cTemp = -45 + (175 * temp / 65535.0)
+fTemp = -49 + (315 * temp / 65535.0)
+humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+
+# Output data to screen
+print "Temperature in Celsius is : %.2f C" %cTemp
+print "Temperature in Fahrenheit is : %.2f F" %fTemp
+print "Relative Humidity is : %.2f %%RH" %humidity
